77d95f83795bae0ecca068148f8feb8569e8d642
Carly Clift <carlymclift@gmail.com>
2021-01-05 14:55:13 -0700

    Iterate through restaurants data from api #1

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Table.css'
+import fakeData from '../fakeData'
+import RestaurantCard from './RestaurantCard'
+import { getRestaurantData } from '../api/apiCalls'
 
 const Table = () => {
+// console.log(fakeData.restaurants)
+
+  const [restaurants, setRestaurants] = useState([])
+
+  useEffect(() => {
+    setRestaurants(fakeData.restaurants)
+
+    // const data = getRestaurantData()
+    // setRestaurants(data)
+  })
+
+  const cards = restaurants.map(restaurants => {
+    return(
+      <RestaurantCard
+        key={restaurants.id}
+        {...restaurants}
+      />
+    )
+  })
 
   return (
     <div className="table-container" >
-        <h2>hi</h2>
-        <h2>hi</h2>
-        <h2>hi</h2>
-        <h2>hi</h2>
-        <h2>hi</h2>
-        <h2>hi</h2>
-        <h2>hi</h2>
-        <h2>hi</h2>
-        <h2>hi</h2>
-        <h2>hi</h2>
+      {cards}
     </div>
   )
 }
