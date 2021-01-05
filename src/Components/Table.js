a086986a2efca97168ebcf0245f019ac3b57e1cf
Carly Clift <carlymclift@gmail.com>
2021-01-05 13:11:49 -0700

    Create table with css grid

---
+++ src/Components/Table.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Table.css'
+
+const Table = () => {
+
+  return (
+    <div className="table-container" >
+        <h2>hi</h2>
+        <h2>hi</h2>
+        <h2>hi</h2>
+        <h2>hi</h2>
+        <h2>hi</h2>
+        <h2>hi</h2>
+        <h2>hi</h2>
+        <h2>hi</h2>
+        <h2>hi</h2>
+        <h2>hi</h2>
+    </div>
+  )
+}
+
+export default Table
