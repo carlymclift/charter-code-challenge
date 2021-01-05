52036f651868ca6e6f0832a2112c8069ea030b57
Carly Clift <carlymclift@gmail.com>
2021-01-05 14:53:10 -0700

    Add content to cards from the data collected #1

---
+++ src/Components/RestaurantCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './RestaurantCard.css'
+
+const RestaurantCard = ({address1, attire, city, genre, hours, id, lat, long, name, state, tags, telephone, website, zip}) => {
+  const openHours = hours.split(';').map(day => (<p key={day} style={{margin: '0'}}>{day}</p>))
+
+  return (
+    <div className='card-box' >
+        <h3 className='restaurant-name' >{name}</h3>
+        <p>{address1} {city}, {state}, {zip}</p>
+        <p>Phone: {telephone}</p>
+        <a target='_blank' rel='noreferrer' href={website} >Website</a>
+        <p>Hours:</p>
+        <div className='hours' >
+          {openHours}
+        </div>
+    </div>
+  )
+}
+
+export default RestaurantCard
